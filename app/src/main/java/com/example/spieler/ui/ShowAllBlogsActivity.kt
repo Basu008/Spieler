cd7adafcfc0c4df32f5195c9a4a34ce9dff02232
Basu Singh <basu.singh@hypd.in>
2022-04-06 02:06:04 +0530

    Added 3 more option in nav drawer

---
+++ app/src/main/java/com/example/spieler/ui/ShowAllBlogsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.spieler.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.spieler.R
+
+class ShowAllBlogsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_all_blogs)
+    }
+}
